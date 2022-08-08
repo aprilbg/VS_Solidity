9c64c21be42a264704104c9f387defb5de708ee2
aprilbg13 <aprilbg13@bloomtechnology.co.kr>
2022-08-08 17:16:15 +0900

    FIX : FIX the code  - change Solidity version 0.5.6 --> 0.8.0  - remove not uising interface & add interface IKIP17Enumerable  -  change Contract name  - remove constructor because solidity version  - remove masger function  - add function (_addTokenToOwnerEnumeration, _removeTokenFromOwnerEnumeration, _tokensOfOwner, tokenOfOwnerByIndex, tokenByIndex, totalSupply)  - msg.sender --> from because solidity version  - add mapping (_ownedTokens, _ownedTokensIndex)  -

--- Solidity_NFT/NFT.sol
+++ Solidity_NFT/NFT.sol
@@ -1,30 +1,29 @@
-pragma solidity ^0.5.6;
-interface ERC165 {
-    function supportsInterface(bytes4 interfaceID) external view returns (bool);
-}
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
 interface ERC721TokenReceiver {
     function onERC721Received(address _operator, address _from, uint256 _tokenId, bytes4 _data) external returns(bytes4);
 }
 
-contract ERC721Implemntation {
+interface IKIP17Enumerable {
+    function tokenOfOwnerByIndex(address owner, uint256 index) external returns(uint256 tokenId);
+}
+
+contract BildNFT {
     mapping (uint256 => address) tokenOwner;
     mapping (address => uint256) ownedTokenCount;
     mapping (address=>mapping(address => bool)) operatorApprovals;
-    mapping(uint256 => address) private tokenApprovals;
+    mapping(uint256 => address) tokenApprovals;
+    mapping(address => uint256[])_ownedTokens;
+    mapping(uint256 => uint256) _ownedTokensIndex;
 
     event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
     event Approval(address indexed _owner, address indexed _approved, uint256 indexed _tokenId);
     event ApprovalForAll(address indexed _owner, address indexed _operator, bool _approved);
 
-    string private _name;
-    string private _symbol;
-    address payable private _master;
-
-    constructor(string memory name, string memory symbol) public {
-        _name = name;
-        _symbol = symbol;
-        _master = msg.sender;
-    }
+    string private _name = "made";
+    string private _symbol = "MADE";
+    uint256[] public _allTokens;
 
     function name() public view returns (string memory){
         return _name;
@@ -34,8 +33,8 @@ contract ERC721Implemntation {
         return _symbol;
     }
 
-    function master() public view returns (address payable){
-        return _master;
+    function totalSupply() public view returns (uint256) {
+        return _allTokens.length;
     }
 
     function mint(address _to, uint _tokenId) public {
@@ -44,6 +43,19 @@ contract ERC721Implemntation {
         tokenOwner[_tokenId] = _to;
         ownedTokenCount[_to] += 1;
         emit Transfer(address(0), _to, _tokenId);
+        _addTokenToOwnerEnumeration(_to, _tokenId);
+        _ownedTokensIndex[_tokenId] = _allTokens.length;
+        _allTokens.push(_tokenId);
+    }
+
+    function tokenByIndex(uint256 index) public view returns (uint256) {
+        require(index < totalSupply());
+        return _allTokens[index];
+    }
+
+    function tokenOfOwnerByIndex(address owner, uint256 index) public view returns (uint256) {
+        require(index < balanceOf(owner));
+        return _ownedTokens[owner][index];
     }
 
     function balanceOf(address _owner) public view returns (uint256) {
@@ -62,15 +74,40 @@ contract ERC721Implemntation {
         return owner != address(0);
     }
 
+    function _tokensOfOwner(address owner) public view returns (uint256[] memory) {
+        return _ownedTokens[owner];
+    }
+
     function transferFrom(address _from, address _to, uint256 _tokenId) internal{
         address owner = ownerOf(_tokenId);
-        require(msg.sender == owner || msg.sender == getApproved(_tokenId));
+        require(_from == owner || _from == getApproved(_tokenId));
         require(_from != address(0) && _to != address(0));
 
-        tokenOwner[_tokenId] = _to;
         ownedTokenCount[_from] -= 1;
         ownedTokenCount[_to] += 1;
+        tokenOwner[_tokenId] = _to;
         emit Transfer(_from, _to, _tokenId);
+        _removeTokenFromOwnerEnumeration(_from, _tokenId);
+        _addTokenToOwnerEnumeration(_to, _tokenId);
+    }
+
+    function _addTokenToOwnerEnumeration(address to, uint256 tokenId) private {
+        _ownedTokensIndex[tokenId] = _ownedTokens[to].length;
+        _ownedTokens[to].push(tokenId);
+    }
+
+    function _removeTokenFromOwnerEnumeration(address from, uint256 tokenId) private{
+        uint256 lastTokenIndex = _ownedTokens[from].length - 1;
+        uint256 tokenIndex = _ownedTokensIndex[tokenId];
+
+        if (tokenIndex != lastTokenIndex){
+            uint256 lastTokenId = _ownedTokens[from][lastTokenIndex];
+
+            _ownedTokens[from][tokenIndex] = lastTokenId;
+            _ownedTokensIndex[lastTokenId] = tokenIndex;
+        }
+        delete _ownedTokensIndex[tokenId];
+        delete _ownedTokens[from][lastTokenIndex];
     }
 
     function safeTransferFrom(address _from, address  _to, uint256 _tokenId) public {
